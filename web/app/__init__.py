a65959f646bcab87b1cef4b0004044d0dacf010b
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2025-08-25 15:10:58 -0700

    Revert "Got flask app workin"
    This reverts commit 2728db885d0a825e74a29fe8a0b7ad201c5e747f.

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -7,7 +7,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name="Vigil")
-
-if __name__ == "__main__": 
-    app.run()
+    return render_template("index.html", name="Flask Svelte")
