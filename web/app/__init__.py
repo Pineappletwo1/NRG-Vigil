2728db885d0a825e74a29fe8a0b7ad201c5e747f
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2025-08-24 17:49:47 -0700

    Got flask app workin

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -7,4 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name="Flask Svelte")
+    return render_template("index.html", name="Vigil")
+
+if __name__ == "__main__": 
+    app.run()
