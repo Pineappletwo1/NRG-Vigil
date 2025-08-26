c2778cc1e6fc4469049224956126f09164cc04c9
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2025-08-25 20:28:31 -0700

    Fixing more stuff I broke

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,20 +1,9 @@
-import asyncio
-from websockets import serve
 from flask import Flask
 
 from flask_svelte import render_template
 
 app = Flask(__name__)
 
-async def test(websocket): 
-    await websocket.recv()
-    websocket.send("test")
-
-async def main(): 
-    async with serve(test, "localhost", 5000) as server: 
-        await server.serve_forever()
-
 @app.route("/")
 def index():
-    asyncio.run(main())
     return render_template("index.html", name="Vigil")
